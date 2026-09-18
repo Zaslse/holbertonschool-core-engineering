98e739efb919ea816d325180c4d8072410138f97
zaslse <zaslse@hotmail.com>
2026-09-18 15:00:20 +0300

    Fix lowercase alphabet output

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-print("".join(chr(letter) for letter in range(ord('a'), ord('z') + 1)
-              if chr(letter) not in "qe"))
+print("{}".format("".join(chr(letter) for letter in range(ord('a'),
+      ord('z') + 1) if chr(letter) not in "qe")))
