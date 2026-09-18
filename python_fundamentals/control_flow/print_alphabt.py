ae71f6137cc5bb366a6f97b5a085518cd89eece5
zaslse <zaslse@hotmail.com>
2026-09-18 15:02:17 +0300

    Fix alphabet output formatting

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 print("{}".format("".join(chr(letter) for letter in range(ord('a'),
-      ord('z') + 1) if chr(letter) not in "qe")))
+      ord('z') + 1) if chr(letter) not in "qe")), end="")
+
