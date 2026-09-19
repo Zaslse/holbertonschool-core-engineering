8cf0c05e326feb2956d711b9341e69ab08aa7f32
zaslse <zaslse@hotmail.com>
2026-09-19 14:07:53 +0300

    Fix matrix integer formatting

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -3,4 +3,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join("{}".format(number) for number in row))
+        print(" ".join("{:d}".format(number) for number in row))
